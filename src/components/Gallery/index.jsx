18ac1b63b2bb8f99efdb4005848cdca85f9ac0dd
Hachi <Hachi@MacBook-Pro.local>
2021-02-19 23:26:35 +0900

    Custom infinate scroll function implemented, added some comments to explain

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,31 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import GalleryImage from '../GalleryImage';
 
 const Gallery = () => {
-  // set inital value of unsplashImages to empty array
+  // State holding the bool value of out loader
+  const [loading, setLoading] = useState(false);
+
+  // State holding the array of images
   const [unsplashImages, setUnsplashImages] = useState([]);
 
-  async function fetchUnsplashImages() {
+  // State holding the page number we want to fetch
+  const [unsplashPage, setUpsplashPage] = useState(1);
+
+  // Function fetches images from unsplash API
+
+  const fetchUnsplashImages = () => {
+    // While fetching we set the loading state to true
+    setLoading(true);
+
     // Send request to unsplash api with access token
-    const response = await fetch(`https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`);
-    // Set response as state
-    setUnsplashImages(await response.json());
-  }
+    // We are also getting images from the next page using the page state
+    fetch(`https://api.unsplash.com/photos/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&count=10&page=${unsplashPage}`)
+
+      .then((response) => response.json())
+      .then((data) => {
+        // Add response to image array
+        setUnsplashImages([...unsplashImages, ...data]);
+
+        // Increase page number by one as that what we will search next time we fetch
+        const newPage = unsplashPage + 1;
+        setUpsplashPage(newPage);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        // We have finished fetching images for now, lets set the loader to false
+        setLoading(false);
+        console.log(unsplashImages);
+      });
+  };
+
   useEffect(() => {
+    // Fetch first page of images on mount
     fetchUnsplashImages();
   }, []);
 
+  // Once we scroll to the bottom run the fetchUnsplashImages function
+  window.onscroll = () => {
+    if (window.innerHeight
+      + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+      fetchUnsplashImages();
+    }
+  };
+
   return (
-    <div className="mp-gallery">
-      {unsplashImages.length ? unsplashImages.map((image) => (
-        <GalleryImage
-          url={image.urls.regular}
-          alt={image.alt_description}
-        />
-      )) : (
-        <p>No Images</p>
-      )}
-    </div>
+    <>
+      <div className="mp-gallery w">
+        {unsplashImages.map((image) => (
+          <GalleryImage
+            key={image.id}
+            url={image.urls.regular}
+            alt={image.alt_description}
+          />
+        ))}
+      </div>
+      {loading ? <div key="1" className="mp-loader">Loading...</div> : null}
+    </>
   );
 };
 
